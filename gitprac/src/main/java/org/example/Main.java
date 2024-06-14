3a8884d819946e745b869f555645483788579eeb
0xc0debabe <kongminoo@naver.com>
2024-06-15 00:31:54 +0900

    메인은 계속 진행한다

--- gitprac/src/main/java/org/example/Main.java
+++ gitprac/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("메인은 그래도 진행");
+        System.out.println("메인은 계속 새 브랜치 영향없이 진행");
     }
 }
