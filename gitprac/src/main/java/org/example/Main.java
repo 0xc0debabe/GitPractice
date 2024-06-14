ffde4984a126b7383cc8b71e3212df6b5cbf153d
0xc0debabe <kongminoo@naver.com>
2024-06-15 00:47:25 +0900

    메인 브랜치 계쏙 변경

--- gitprac/src/main/java/org/example/Main.java
+++ gitprac/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("메인은 계속 새 브랜치 영향없이 진행");
-        System.out.println("새로운 기능 추가");
+        System.out.println("충돌이 날까");
+        System.out.println("메인브랜치");
     }
 }
