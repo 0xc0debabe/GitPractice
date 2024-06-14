b86dfe7d7f2693c9f9fc77a93d9cc858ea78a703
0xc0debabe <kongminoo@naver.com>
2024-06-15 00:46:24 +0900

    새기능커밋

--- gitprac/src/main/java/org/example/Main.java
+++ gitprac/src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("메인은 그래도 진행");
         System.out.println("새로운 기능 추가");
         System.out.println("충돌예정");
+        System.out.println("충돌예정2");
+        System.out.println("충돌예정3");
     }
 }
